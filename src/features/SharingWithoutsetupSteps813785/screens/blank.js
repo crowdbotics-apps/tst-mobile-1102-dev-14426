5876aa510cea3b105c9a820d52cd2a019605ab38
Crowdbotics <team@crowdbotics.com>
2020-11-03 22:24:23 +0000

    add component Sharing withoutsetup steps

---
+++ src/features/SharingWithoutsetupSteps813785/screens/blank.js
@@ -0,0 +1,42 @@
+import React from "react"
+import {
+  View,
+  Image,
+  ImageBackground,
+  TouchableOpacity,
+  Text,
+  Button,
+  Switch,
+  TextInput,
+  StyleSheet
+} from "react-native"
+import DateTimePicker from "react-native-datepicker"
+import Icon from "react-native-vector-icons/FontAwesome"
+import Slider from "@react-native-community/slider"
+import { CheckBox } from "react-native-elements"
+import { SlideMenuIcon } from "../../../navigator/slideMenuIcon"
+
+export default class Blank extends React.Component {
+  static navigationOptions = ({ navigation }) => {
+    return {
+      headerLeft: <SlideMenuIcon navigationProps={navigation} />
+    }
+  }
+
+  state = {}
+
+  render = () => (
+    <View>
+      <Text>Sample text &lt;br /&gt; content</Text>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    marginHorizontal: 16
+  },
+  View_1: {},
+  Text_3: {}
+})
