5622d8a31f015e2ca5e535fcebe86931e9ce7713
Crowdbotics <team@crowdbotics.com>
2020-11-05 18:55:09 +0000

    Update components

--- src/features/BlankScreen013701/screens/blank.js
+++ src/features/BlankScreen013701/screens/blank.js
@@ -23,11 +23,47 @@ export default class Blank extends React.Component {
     }
   }
 
-  state = {}
+  state = {
+    Switch_3: true,
+    CheckBox_17: true,
+    CheckBox_5: true,
+    TextInput_7: "",
+    TextInput_9: ""
+  }
 
   render = () => (
     <View>
-      <Text>Sample text &lt;br /&gt; content</Text>
+      <Switch
+        activeColor="#409EFF"
+        inactiveColor="#C0CCDA"
+        disabled={false}
+        trackColor={{ false: "#C0CCDA", true: "#409EFF" }}
+        value={this.state.Switch_3}
+        onValueChange={nextChecked => this.setState({ Switch_3: nextChecked })}
+      />
+      <CheckBox
+        title="Radio button"
+        checkedIcon="dot-circle-o"
+        uncheckedIcon="circle-o"
+        checked={this.state.CheckBox_17}
+        onPress={nextChecked => this.setState({ CheckBox_17: nextChecked })}
+      />
+      <CheckBox
+        title="Checkbox"
+        checked={this.state.CheckBox_5}
+        onPress={nextChecked => this.setState({ CheckBox_5: nextChecked })}
+      />
+      <TextInput
+        placeholder="Sample text input placeholder"
+        value={this.state.TextInput_7}
+        onChangeText={nextValue => this.setState({ TextInput_7: nextValue })}
+      />
+      <TextInput
+        placeholder="Number Input Placeholder"
+        keyboardType="numeric"
+        value={this.state.TextInput_9}
+        onChangeText={nextValue => this.setState({ TextInput_9: nextValue })}
+      />
     </View>
   )
 }
@@ -37,6 +73,13 @@ const styles = StyleSheet.create({
     flex: 1,
     marginHorizontal: 16
   },
+
   View_1: {},
-  Text_3: {}
+  Text_3: {},
+  View_1: {},
+  Switch_3: {},
+  CheckBox_17: {},
+  CheckBox_5: {},
+  TextInput_7: {},
+  TextInput_9: {}
 })
